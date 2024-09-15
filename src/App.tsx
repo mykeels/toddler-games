1b7fc265073c91a3b84edc661acc80e9d13d333b
mykeels <mykehell123@gmail.com>
2024-09-15 12:31:45 -0400

    chore: show app version

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import classNames from "clsx";
 import "./App.css";
 
 function App() {
+  const pkgVersion = __APP_VERSION__ as unknown as string;
   const [state, setState] = useState<"playing" | "interlude">("playing");
   const [pair, setPair] = useState<string[]>(getRandomPair());
   const [selected, setSelected] = useState<string | null>(null);
@@ -22,50 +23,55 @@ function App() {
 
   return (
     <>
-      {state === "playing" ? (
-        <h1 className="text-4xl text-gray-800">
-          What is this?
-        </h1>
-      ) : null}
-      <div data-name="pair" className="flex justify-center space-x-8 mt-8">
-        <button
-          onMouseDown={() => onLetterOrNumberClick(pair[0])}
-          className={classNames(
-            "w-72 border-8 border-gray-800 flex items-center justify-center text-9xl font-bold",
-            {
-              "bg-blue-200 animate-breathe": selected === pair[0],
-              "h-72": state !== "interlude",
-            }
-          )}
-        >
-          {pair[0]}
-        </button>
-        <button
-          onMouseDown={() => onLetterOrNumberClick(pair[1])}
-          className={classNames(
-            "w-72 border-8 border-gray-800 flex items-center justify-center text-9xl font-bold",
-            {
-              "bg-blue-200 animate-breathe": selected === pair[1],
-              "h-72": state !== "interlude",
-            }
-          )}
-        >
-          {pair[1]}
-        </button>
+      <div className="flex flex-col h-screen">
+        <main className="flex-grow max-h-[90vh]">
+          {state === "playing" ? (
+            <h1 className="text-4xl text-gray-800">What is this?</h1>
+          ) : null}
+          <div data-name="pair" className="flex justify-center space-x-8 mt-8">
+            <button
+              onMouseDown={() => onLetterOrNumberClick(pair[0])}
+              className={classNames(
+                "w-72 border-8 border-gray-800 flex items-center justify-center text-9xl font-bold",
+                {
+                  "bg-blue-200 animate-breathe": selected === pair[0],
+                  "h-72": state !== "interlude",
+                }
+              )}
+            >
+              {pair[0]}
+            </button>
+            <button
+              onMouseDown={() => onLetterOrNumberClick(pair[1])}
+              className={classNames(
+                "w-72 border-8 border-gray-800 flex items-center justify-center text-9xl font-bold",
+                {
+                  "bg-blue-200 animate-breathe": selected === pair[1],
+                  "h-72": state !== "interlude",
+                }
+              )}
+            >
+              {pair[1]}
+            </button>
+          </div>
+          {state == "interlude" ? (
+            <div
+              data-name="interlude"
+              className="flex flex-col items-center justify-center py-24 space-y-8"
+            >
+              <button
+                onMouseDown={onNextClick}
+                className="px-16 py-8 text-8xl text-gray-800 border-8 border-gray-800 p-4 rounded-md"
+              >
+                Next
+              </button>
+            </div>
+          ) : null}
+        </main>
+        <footer className="text-gray-800 text-sm text-center">
+          {pkgVersion}
+        </footer>
       </div>
-      {state == "interlude" ? (
-        <div
-          data-name="interlude"
-          className="flex flex-col items-center justify-center py-24 space-y-8"
-        >
-          <button
-            onMouseDown={onNextClick}
-            className="px-16 py-8 text-8xl text-gray-800 border-8 border-gray-800 p-4 rounded-md"
-          >
-            Next
-          </button>
-        </div>
-      ) : null}
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
+});
