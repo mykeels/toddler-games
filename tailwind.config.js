838d7e0a99ec30733c0bbf0296c95f31529c6d0d
mykeels <mykehell123@gmail.com>
2024-11-06 22:35:56 -0500

    feat: setup shadcn ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      sans: ['Segoe UI', 'San Francisco', 'Helvetica Neue', 'Arial', 'sans-serif'],
-    },
-    extend: {
-      colors: {
-        brand: {
-          primary: '#BB017A',
-          pink: {
-            100: '#C62B90',
-            200: '#D256A6',
-            300: '#DD80BD',
-            400: '#EBAAD3',
-            500: '#F1CCE4',
-          },
-          accent: {
-            orange: '#FF5117',
-            pink: '#FF1093',
-            blue: '#17A6FF',
-            yellow: '#FFC300',
-            green: '#17FF70',
-            purple: '#803EC2'
-          }
-        }
-      }
-    },
+  	fontFamily: {
+  		sans: ['Segoe UI', 'San Francisco', 'Helvetica Neue', 'Arial', 'sans-serif']
+  	},
+  	extend: {
+  		colors: {
+  			brand: {
+  				primary: '#BB017A',
+  				pink: {
+  					'100': '#C62B90',
+  					'200': '#D256A6',
+  					'300': '#DD80BD',
+  					'400': '#EBAAD3',
+  					'500': '#F1CCE4'
+  				},
+  				accent: {
+  					orange: '#FF5117',
+  					pink: '#FF1093',
+  					blue: '#17A6FF',
+  					yellow: '#FFC300',
+  					green: '#17FF70',
+  					purple: '#803EC2'
+  				}
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
