c3adad47b7ecffdba1bfee81eb0c00afd01b612c
mykeels <mykehell123@gmail.com>
2024-11-06 20:46:25 -0500

    feat: add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Segoe UI', 'San Francisco', 'Helvetica Neue', 'Arial', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        brand: {
+          primary: '#BB017A',
+          pink: {
+            100: '#C62B90',
+            200: '#D256A6',
+            300: '#DD80BD',
+            400: '#EBAAD3',
+            500: '#F1CCE4',
+          },
+          accent: {
+            orange: '#FF5117',
+            pink: '#FF1093',
+            blue: '#17A6FF',
+            yellow: '#FFC300',
+            green: '#17FF70',
+            purple: '#803EC2'
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
