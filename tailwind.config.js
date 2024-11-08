a92feecdbdd32b8e012e961de6f8abd194f4de1b
mykeels <mykehell123@gmail.com>
2024-11-08 06:16:44 -0500

    feat: add screen target for retina displays

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	fontFamily: {
-  		lily: ['Lily Script One', 'Poppins', 'Segoe UI', 'San Francisco', 'Helvetica Neue', 'Arial', 'sans-serif']
-  	},
-  	extend: {
-  		colors: {
-  			brand: {
-  				primary: '#BB017A',
-  				pink: {
-  					'100': '#C62B90',
-  					'200': '#D256A6',
-  					'300': '#DD80BD',
-  					'400': '#EBAAD3',
-  					'500': '#F1CCE4'
-  				},
-  				accent: {
-  					orange: '#FF5117',
-  					pink: '#FF1093',
-  					blue: '#17A6FF',
-  					yellow: '#FFC300',
-  					green: '#17FF70',
-  					purple: '#803EC2'
-  				}
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    fontFamily: {
+      lily: [
+        "Lily Script One",
+        "Poppins",
+        "Segoe UI",
+        "San Francisco",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif",
+      ],
+    },
+    extend: {
+      colors: {
+        brand: {
+          primary: "#BB017A",
+          pink: {
+            100: "#C62B90",
+            200: "#D256A6",
+            300: "#DD80BD",
+            400: "#EBAAD3",
+            500: "#F1CCE4",
+          },
+          accent: {
+            orange: "#FF5117",
+            pink: "#FF1093",
+            blue: "#17A6FF",
+            yellow: "#FFC300",
+            green: "#17FF70",
+            purple: "#803EC2",
+          },
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      screens: {
+        retina: {
+          raw: "@media not all and (-webkit-min-device-pixel-ratio: 2), (min-device-pixel-ratio: 2), (min-resolution: 192dpi)",
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
-}
-
+};
