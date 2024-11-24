b6f2555c295150ca1684f9c90f2f759a9ed0939a
mykeels <mykehell123@gmail.com>
2024-11-24 06:20:55 -0500

    feat: change letter-tracing color

--- src/games/LetterTracing/LetterTracing.consts.ts
+++ src/games/LetterTracing/LetterTracing.consts.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_LETTER_SIZE = 300;
-export const DEFAULT_DOT_SIZE = 3;
-export const DEFAULT_COLOR = "black";
+export const DEFAULT_DOT_SIZE = 2;
+export const DEFAULT_COLOR = "#aaa";
