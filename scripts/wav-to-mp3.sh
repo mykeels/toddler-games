0dcf9142f4f4d406488b4d3377fe19f1c1aede87
mykeels <mykehell123@gmail.com>
2024-11-24 08:12:09 -0500

    chore: convert alphabet wav to mp3

--- scripts/wav-to-mp3.sh
+++ scripts/wav-to-mp3.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
-letters=("c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
+letters=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
 
-rm public/soundfx/phonics/d.wav
-rm public/soundfx/phonics/e.wav
-rm public/soundfx/phonics/f.wav
-rm public/soundfx/phonics/g.wav
-rm public/soundfx/phonics/h.wav
-rm public/soundfx/phonics/i.wav
-rm public/soundfx/phonics/j.wav
-rm public/soundfx/phonics/k.wav
-rm public/soundfx/phonics/l.wav
-rm public/soundfx/phonics/m.wav
-rm public/soundfx/phonics/n.wav
-rm public/soundfx/phonics/o.wav
-rm public/soundfx/phonics/p.wav
-rm public/soundfx/phonics/q.wav
-rm public/soundfx/phonics/r.wav
-rm public/soundfx/phonics/s.wav
-rm public/soundfx/phonics/t.wav
-rm public/soundfx/phonics/u.wav
-rm public/soundfx/phonics/v.wav
-rm public/soundfx/phonics/w.wav
-rm public/soundfx/phonics/x.wav
-rm public/soundfx/phonics/y.wav
-rm public/soundfx/phonics/z.wav
+for letter in $letters; do
+    rm public/soundfx/alphabet/$letter.wav
+done
