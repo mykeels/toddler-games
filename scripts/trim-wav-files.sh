795a2429cfd1ab56744add685006047d1a54cd61
mykeels <mykehell123@gmail.com>
2024-11-24 08:10:10 -0500

    chore: trim alphabet wav files

--- scripts/trim-wav-files.sh
+++ scripts/trim-wav-files.sh
@@ -3,8 +3,8 @@
 ## This script trims the wav files for the phonics game.
 ## Uses sox to trim the files.
 
-letters=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+letters=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
 
 for letter in $letters; do
-  sox public/soundfx/digits/originals/$letter.wav public/soundfx/digits/$letter.wav silence 1 0.1 1% reverse silence 1 0.1 1% reverse
+  sox public/soundfx/alphabet/originals/$letter.wav public/soundfx/alphabet/$letter.wav silence 1 0.1 1% reverse silence 1 0.1 1% reverse
 done
