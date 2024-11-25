a9ac7e8564dd296343f165b967cd453d2dc62dec
mykeels <mykehell123@gmail.com>
2024-11-25 14:42:03 -0500

    feat: debounce set level

--- src/Header/Levels/index.tsx
+++ src/Header/Levels/index.tsx
@@ -2,13 +2,20 @@
 import clsx from "clsx";
 import { useToggleMenu } from "../useToggleMenu";
 import { onTouch } from "@/utils/touch";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { StoryFn } from "@storybook/react";
+import { useDebouncedCallback } from 'use-debounce';
 
 export const Levels = () => {
     const { isOpen, toggleMenu } = useToggleMenu({ open: false });
     const { level, setLevel } = useContext(LevelContext);
+    const titleRef = useRef<HTMLHeadingElement>(null);
+    const updateTitle = (value: number) => {
+        if (titleRef.current) {
+            titleRef.current.textContent = `Level ${value}`;
+        }
+    }
     return <div className="flex flex-row gap-2 relative">
         <button {...onTouch(toggleMenu)}>
             <img src="./icons/ladder-white.svg" alt="ladder" className={
@@ -32,14 +39,17 @@ export const Levels = () => {
                 }}
                 className="absolute right-0 top-[-50%] w-48 bg-brand-primary p-1 mr-8 rounded flex flex-col gap-2"
             >
-                <h4 className="text-white text-sm text-center">{`Level ${level}`}</h4>
+                <h4 className="text-white text-sm text-center" ref={titleRef}>{`Level ${level}`}</h4>
                 <input
                     type="range"
                     min="1"
                     max="5"
                     step="1"
-                    value={level}
-                    onChange={(e) => setLevel(parseInt(e.target.value))}
+                    defaultValue={level.toString()}
+                    onChange={(e) => {
+                        setLevel(parseInt(e.target.value));
+                        updateTitle(parseInt(e.target.value));
+                    }}
                     className="w-full cursor-pointer"
                 />
             </motion.div>
@@ -64,11 +74,12 @@ export const LevelContextProvider = ({
     saveLevel?: (level: number) => void
 }) => {
     const [level, setLevel] = useState(getLevel ? getLevel() : 1);
+    const debouncedSetLevel = useDebouncedCallback((level: number) => {
+        setLevel(level);
+        if (saveLevel) saveLevel(level);
+    }, 1000);
     return <LevelContext.Provider value={{
-        level, setLevel: level => {
-            setLevel(level);
-            if (saveLevel) saveLevel(level);
-        }
+        level, setLevel: level => debouncedSetLevel(level)
     }}>
         {children}
     </LevelContext.Provider>
