b6e9b9a0e5b7805da2b734987372266813628c7c
mykeels <mykehell123@gmail.com>
2025-06-11 00:48:47 -0400

    fix: getBaseUrl

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,7 @@
+import urlJoin from "url-join";
+
 export const getBaseUrl = () => {
+  const assetUrl = import.meta.env.BASE_URL || import.meta.env.ASSET_URL;
   if (typeof document !== "undefined") {
     const currentScript = (document.currentScript ||
       document.querySelector(
@@ -7,20 +10,16 @@ export const getBaseUrl = () => {
 
     const entryUrl =
       currentScript?.getAttribute("src") || new URL(import.meta.url).origin;
-    const baseUrl =
-      entryUrl
-        .split("?")[0]
-        ?.replace("/dist/assets/remoteEntry.js", "")
-        .replace("/assets/remoteEntry.js", "") || "/";
+    const baseUrl = new URL(entryUrl).origin;
 
-    if (import.meta.env.ASSET_URL) {
-      return `${baseUrl}/${import.meta.env.ASSET_URL}`;
+    if (assetUrl && !baseUrl.endsWith(assetUrl)) {
+      return urlJoin(baseUrl, assetUrl);
     }
     return baseUrl;
   }
   const entryUrl = new URL(import.meta.url).origin;
-  if (import.meta.env.ASSET_URL) {
-    return `${entryUrl}/${import.meta.env.ASSET_URL}`;
+  if (assetUrl && !entryUrl.endsWith(assetUrl)) {
+    return urlJoin(entryUrl, assetUrl);
   }
   return "/";
 };
