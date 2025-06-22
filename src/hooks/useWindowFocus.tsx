ae4bc7d0e5c75b1381885172feb52a337230943f
mykeels <mykehell123@gmail.com>
2025-06-22 11:32:21 -0400

    feat: enhance theme music functionality with volume control and window focus handling

---
+++ src/hooks/useWindowFocus.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export const useWindowFocus = ({
+  onFocus,
+  onBlur,
+  onFocusChange,
+}: {
+  onFocus?: () => void;
+  onBlur?: () => void;
+  onFocusChange?: (isFocused: boolean) => void;
+}) => {
+  const [isFocused, setIsFocused] = useState(true);
+  useEffect(() => {
+    const handleFocus = () => {
+      console.log("Window focused");
+      setIsFocused(true);
+      onFocus?.();
+      onFocusChange?.(true);
+    };
+    const handleBlur = () => {
+      console.log("Window blurred");
+      setIsFocused(false);
+      onBlur?.();
+      onFocusChange?.(false);
+    };
+    window.addEventListener("focus", handleFocus);
+    window.addEventListener("blur", handleBlur);
+  }, []);
+  return { isFocused };
+};
--- src/utils/theme-music.tsx
+++ src/utils/theme-music.tsx
@@ -1,13 +1,43 @@
 import { useEffect } from "react";
 import { fx } from "./sound";
+import { useWindowFocus } from "@/hooks/useWindowFocus";
+import { sleep } from "./sleep";
+
+const DEFAULT_MUSIC_VOLUME = 0.3;
 
 export const ThemeMusic = () => {
-  useEffect(() => {
-    fx.theme.volume(0.3);
+  const play = () => {
+    fx.theme.volume(DEFAULT_MUSIC_VOLUME);
     fx.theme.loop(true).play();
+  };
+  const stop = () => {
+    graduallyReduceVolumeThenStop(fx.theme);
+  };
+  useWindowFocus({
+    onFocus: () => {
+      play();
+    },
+    onBlur: () => {
+      stop();
+    },
+  });
+  useEffect(() => {
+    play();
     return () => {
-      fx.theme.stop();
+      stop();
     };
   }, []);
   return null;
+};
+
+const graduallyReduceVolumeThenStop = async (audio: Howl) => {
+  const originalVolume = audio.volume();
+  const step = originalVolume / 15;
+  const interval = setInterval(() => {
+    audio.volume(audio.volume() - step);
+  }, 100);
+  await sleep(1500);
+  clearInterval(interval);
+  audio.volume(0);
+  audio.stop();
 };
