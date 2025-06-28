ddd3a4032c6b554e521830cdeb75a905b1908d0c
mykeels <mykehell123@gmail.com>
2025-06-28 18:42:09 -0400

    feat: add Play All button and functionality to NumberKeypad component

--- src/games/NumberKeypad/index.tsx
+++ src/games/NumberKeypad/index.tsx
@@ -6,6 +6,10 @@ import Container from '@/Container';
 import { vibrate } from '@/utils/vibrate';
 import { useSpeak } from '@/utils/speak';
 import README from './README.md';
+import { sleep } from '@/utils/sleep';
+import { useQuery } from 'react-query';
+
+const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 export const NumberKeypad = () => {
   const { speak } = useSpeak();
@@ -44,22 +48,36 @@ export const NumberKeypad = () => {
     return () => controller.abort();
   }, [dial]);
 
+  const playAll = async () => {
+    for (const digit of DIGITS) {
+      const button = document.querySelector(`[data-digit="${digit}"]`);
+      (button as HTMLButtonElement)?.click();
+      await sleep(700);
+    }
+  };
+  const {
+    refetch: playAllDigits,
+    isRefetching,
+    isFetching,
+    isLoading,
+  } = useQuery({
+    queryKey: ['number-keypad'],
+    queryFn: async () => playAll(),
+    enabled: false,
+  });
+  const isPlaying = isRefetching || isFetching || isLoading;
+
   return (
     <Container>
       <Header onRestart={undefined} noLevels Right={<Header.Info description={README} />}>
         {recipient || '#'}
       </Header>
       <div className="flex flex-wrap justify-center content-center items-center gap-4 landscape:px-[10%] h-[90%]">
-        <DigitButton value="0" onClick={() => dial('0')} />
-        <DigitButton value="1" onClick={() => dial('1')} />
-        <DigitButton value="2" onClick={() => dial('2')} />
-        <DigitButton value="3" onClick={() => dial('3')} />
-        <DigitButton value="4" onClick={() => dial('4')} />
-        <DigitButton value="5" onClick={() => dial('5')} />
-        <DigitButton value="6" onClick={() => dial('6')} />
-        <DigitButton value="7" onClick={() => dial('7')} />
-        <DigitButton value="8" onClick={() => dial('8')} />
-        <DigitButton value="9" onClick={() => dial('9')} />
+        <PlayAllButton onClick={() => playAllDigits()} isPlaying={isPlaying} />
+
+        {DIGITS.map((digit, index) => (
+          <DigitButton key={`${digit}-${index}`} value={digit} onClick={() => dial(digit)} />
+        ))}
       </div>
     </Container>
   );
@@ -87,6 +105,31 @@ function DigitButton({ value, onClick }: { value: string; onClick: (value: strin
       )}
     >
       {value}
+    </button>
+  );
+}
+
+function PlayAllButton({ onClick, isPlaying }: { onClick: () => void; isPlaying: boolean }) {
+  const [clicked, setClicked] = useState(false);
+  return (
+    <button
+      onClick={() => {
+        onClick();
+        vibrate();
+        setClicked(true);
+        setTimeout(() => setClicked(false), 700);
+      }}
+      className={classNames(
+        'w-24 h-24 lg:w-32 lg:h-32 border-2 border-black flex items-center justify-center text-3xl text-black font-bold rounded',
+        {
+          'bg-white': !clicked && !isPlaying,
+          'bg-brand-accent-yellow text-black animate-breathe': clicked,
+          'bg-brand-accent-green opacity-50': isPlaying,
+        }
+      )}
+      disabled={isPlaying}
+    >
+      <img src="/icons/play-fill.svg" className="w-6 h-6" />
     </button>
   );
 }
