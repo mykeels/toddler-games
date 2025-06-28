1c7fe93dd6774a15315fb69e5ba6bff361021e63
mykeels <mykehell123@gmail.com>
2025-06-28 18:38:03 -0400

    feat: implement Play All functionality in LetterKeypad with loading state

--- src/games/LetterKeypad/index.tsx
+++ src/games/LetterKeypad/index.tsx
@@ -7,6 +7,8 @@ import { vibrate } from '@/utils/vibrate';
 import { useSpeak } from '@/utils/speak';
 import README from './README.md';
 import { LETTERS } from '@/utils/characters';
+import { sleep } from '@/utils/sleep';
+import { useQuery } from 'react-query';
 
 const LETTER_VALUES = LETTERS.map((letter) => letter.value);
 
@@ -46,15 +48,41 @@ export const LetterKeypad = ({ shouldPlayPhonics = false }: { shouldPlayPhonics?
     return () => controller.abort();
   }, [dial]);
 
+  const playAll = async () => {
+    for (const letter of LETTER_VALUES) {
+      const button = document.querySelector(`[data-letter="${letter.toLowerCase()}"]`);
+      (button as HTMLButtonElement)?.click();
+      await sleep(700);
+    }
+    console.log('done');
+  };
+  const {
+    refetch: playAllLetters,
+    isRefetching,
+    isFetching,
+    isLoading,
+  } = useQuery({
+    queryKey: ['letter-keypad'],
+    queryFn: async () => playAll(),
+    enabled: false,
+  });
+
+  const isPlaying = isRefetching || isFetching || isLoading;
+
+  console.log({ isPlaying });
+
   return (
     <Container>
       <Header onRestart={undefined} noLevels Right={<Header.Info description={README} />}>
         {recipient || '#'}
       </Header>
       <div className="flex flex-wrap justify-center content-center items-center gap-4 landscape:px-[10%] h-[90%]">
-        {LETTER_VALUES.map((letter, index) => (
-          <LetterButton key={`${letter}-${index}`} value={letter} onClick={() => dial(letter)} />
-        ))}
+        <PlayAllButton onClick={() => playAllLetters()} isPlaying={isPlaying} />
+        <>
+          {LETTER_VALUES.map((letter, index) => (
+            <LetterButton key={`${letter}-${index}`} value={letter} onClick={() => dial(letter)} />
+          ))}
+        </>
       </div>
     </Container>
   );
@@ -82,6 +110,31 @@ function LetterButton({ value, onClick }: { value: string; onClick: (value: stri
       )}
     >
       {value}
+    </button>
+  );
+}
+
+function PlayAllButton({ onClick, isPlaying }: { onClick: () => void; isPlaying: boolean }) {
+  const [clicked, setClicked] = useState(false);
+  return (
+    <button
+      onClick={() => {
+        onClick();
+        vibrate();
+        setClicked(true);
+        setTimeout(() => setClicked(false), 700);
+      }}
+      className={classNames(
+        'w-12 h-12 lg:w-24 lg:h-24 border-2 border-black flex items-center justify-center text-xl text-black font-bold rounded',
+        {
+          'bg-white': !clicked && !isPlaying,
+          'bg-brand-accent-yellow text-black animate-breathe': clicked,
+          'bg-brand-accent-green opacity-50': isPlaying,
+        }
+      )}
+      disabled={isPlaying}
+    >
+      <img src="/icons/play-fill.svg" className="w-6 h-6" />
     </button>
   );
 }
