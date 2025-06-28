55f5fb095209c58dcf1c1a8c16c1406acbb7eed8
mykeels <mykehell123@gmail.com>
2025-06-28 16:46:10 -0400

    Updates

---
+++ assets/__federation_expose_Index-BhrhseVz.js
@@ -0,0 +1,1 @@
+import{j as t,A as r}from"./main.app-C_yF8Y7m.js";import{g as o}from"./mfe.utils-CapiW-Mo.js";const a=o(()=>t.jsx(r,{}));export{a as default};
--- assets/__federation_expose_Index-CSoxnkUq.js
+++
@@ -1,1 +0,0 @@
-import{j as t,A as r}from"./main.app-CzTcpmdw.js";import{g as o}from"./mfe.utils-B6oY-H0t.js";const a=o(()=>t.jsx(r,{}));export{a as default};
