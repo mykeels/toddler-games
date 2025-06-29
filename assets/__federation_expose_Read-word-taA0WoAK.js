746c1bd7e3ed8f6cc51f181df989d757c8aec45f
mykeels <mykehell123@gmail.com>
2025-06-29 01:10:42 -0400

    Updates

--- assets/__federation_expose_Read-word-taA0WoAK.js
+++
@@ -1,1 +0,0 @@
-import{j as t,a as r,c as a}from"./main.app-B51YZ-Ml.js";import{g as e}from"./mfe.utils-BznyTBoY.js";const d=e(({mountProps:o})=>t.jsx(r,{children:t.jsx(a,{goal:o.goal,standalone:!0})}));export{d as default};
